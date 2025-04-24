08babbfa490e8f239dbb07d61e9e50c0f08221ed
PC-SECMA <PC-SECMA@PC-SECMA>
2025-04-24 13:02:32 -0300

    Aulas 1 a 3

---
+++ #3 -Tipos de operadores/operadores_aritimeticos.py
@@ -0,0 +1,10 @@
+valor_1= 20
+valor_2= 10
+
+print(valor_1 + valor_2)
+print(valor_1 * valor_2)
+print(valor_1 - valor_2)
+print(valor_1 / valor_2)
+print(valor_1 // valor_2) #Divisao inteira
+print(valor_1 % valor_2) #Modulo - Resto da divisao
+print(valor_1 ** valor_2) #Exponenciação
---
+++ #3 -Tipos de operadores/operadores_atribuicao.py
@@ -0,0 +1,8 @@
+saldo = 200
+saldo += 100
+saldo -= 100
+saldo *= 100
+saldo /= 100
+saldo //= 100
+saldo %= 100
+print(saldo)
