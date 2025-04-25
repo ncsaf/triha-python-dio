c566bb3a281d274bd6dde5fd27e72d20d1174e70
PC-SECMA <PC-SECMA@PC-SECMA>
2025-04-25 12:55:04 -0300

    Aulas 3 e 4 - 25/04/2025

---
+++ #3 -Tipos de operadores/operadores_identidade.py
@@ -0,0 +1,5 @@
+saldo = 1000
+saque = 1000
+
+print(saldo is saque) #is verifica se as variáveis ocupam o mesmo espaço de memoria
+print(saldo is not saque) #is not verifica se as variáveis nao ocupam o mesmo espaço de memoria
---
+++ #3 -Tipos de operadores/operadores_logicos.py
@@ -0,0 +1,6 @@
+print(True and True)
+print(True or False)
+print(True and False)
+print(True or False)
+print(True and True)
+print(not True)
