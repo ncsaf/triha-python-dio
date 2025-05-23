ea1675def5d270ead1c03f3dd54b63c125b06ebb
PC-SECMA <PC-SECMA@PC-SECMA>
2025-05-23 11:03:35 -0300

    Finalizado o Projeto do Banco V2

--- projeto-python-banco/banco_python_v2.py
+++ projeto-python-banco/banco_python_v2.py
@@ -1,17 +1,17 @@
-menu = """
+def menu():
+    menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
+    [nu] Novo usuario
+    [nc] Nova Conta
+    [lc] Listar Conta
 
-=> """
+    => """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+    return menu
 
 def sacar(*,saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES):
     if valor > saldo:
@@ -44,37 +44,86 @@ def extratos(saldo,*, extrato):
     print(f"\nSaldo: R$ {saldo:.2f}")
     print("==========================================")
 
-def criar_usuario():
-    nome = input("Digite seu Nome: "),
-    data_nascimento = input("Digite sua Data de Nascimento: "),
-    cpf = int(input("Digite seu CPF: ")),
+def criar_usuario(usuarios):
+    cpf = input("Digite seu CPF: ")
+    verifica_usuario = validar_cpf(cpf,usuarios)
+
+    if verifica_usuario:
+        print("Usuario já cadastrado!")
+        return
+    
+    nome = input("Digite seu Nome: ")
+    data_nascimento = input("Digite sua Data de Nascimento: ")
     endereco = input("Digite seu endereco: ")
 
-    usuario = { cpf: {nome,cpf,data_nascimento,endereco} }
+    usuarios.append({"nome":nome, "data_nascimento":data_nascimento, "cpf":cpf, "endereco":endereco})  
+    
+def validar_cpf(cpf,usuarios):
+    usuario_validado = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuario_validado[0] if usuario_validado else None
 
-    print(usuario)
-       
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o número do CPF (Somente numeros): ")
+    usuario_validado = validar_cpf(cpf, usuarios)
 
+    if usuario_validado:
+        print("Conta Criada com Sucesso!")
+        return {"agencia":agencia , "numero_conta":numero_conta, "usuario":usuario_validado}
+    
+    print("Usuario nao encontrado!")
 
-while True:
-    opcao = input(menu)
+def listar_contas(contas):
+    for conta in contas:
+        print(f"""
+        Agencia:\t{conta["agencia"]}
+        Numero da Conta:{conta["numero_conta"]}
+        Nome:\t\t{conta["usuario"]["nome"]}
+    """)
 
-    if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
+def main():
+    AGENCIA = "001"
+    LIMITE_SAQUES = 3
 
-        saldo, extrato = depositar(saldo, valor, extrato)
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
 
+    while True:
+        opcao = input(menu())
 
-    elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
+        if opcao == "d":
+            valor = float(input("Informe o valor do depósito: "))
 
-        saldo, extrato = sacar(saldo=saldo, valor=valor, extrato=extrato, limite=limite, numero_saques=numero_saques, LIMITE_SAQUES=LIMITE_SAQUES)
+            saldo, extrato = depositar(saldo, valor, extrato)
 
-    elif opcao == "e":
-        extratos(saldo, extrato=extrato)
+        elif opcao == "s":
+            valor = float(input("Informe o valor do saque: "))
 
-    elif opcao == "q":
-        break
+            saldo, extrato = sacar(saldo=saldo, valor=valor, extrato=extrato, limite=limite, numero_saques=numero_saques, LIMITE_SAQUES=LIMITE_SAQUES)
 
-    else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        elif opcao == "e":
+            extratos(saldo, extrato=extrato)
+
+        elif opcao == "nu":
+            criar_usuario(usuarios)
+
+        elif opcao == "nc":
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+        
+        elif opcao == "lc":
+            listar_contas(contas)
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+main()
